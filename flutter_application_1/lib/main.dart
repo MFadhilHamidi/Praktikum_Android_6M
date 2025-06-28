ba4f81ca9eda04881f74f581004efe7ca9a16790
MFadhilHamidi <154021183+MFadhilHamidi@users.noreply.github.com>
2025-06-28 10:15:40 +0800

    PratikumAndroid6M3

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,26 +1,19 @@
 import 'package:flutter/material.dart';
+import 'agenda_list.dart';
 
-void main()  {
-  runApp(MyApp());
+void main() {
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Muhammad Fadhil Hamidi',
-            style: TextStyle(color: Colors.white),
-          ),
-          centerTitle: true,
-          backgroundColor: Colors.blue,
-        ),
-        body: Center(
-          child: Text('Halo Selamat Datang di flutter'),
-        ),
-      ),
+      title: 'Agenda App',
+      theme: ThemeData(primarySwatch: Colors.teal),
+      home: const AgendaList(),
     );
   }
 }
